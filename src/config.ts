ce4f0e18e6db90286326144e65e23943bfc9cf25
PossiblyKevin <kevingarboyan9@gmail.com>
2026-09-03 19:29:14 -0400

    Set default API key and secret in config

--- src/config.ts
+++ src/config.ts
@@ -1,6 +1,6 @@
 export const DEFAULT_CONFIG = {
-  apiKey: "",
-  apiSecret: "",
+  apiKey: "HL7McJBhY6Q2TS6WkZ1OSyhweowYajpC3zH505eOVRddlwhMWR4rXfQWCjyBLbK1",
+  apiSecret: "piig5oupONuDYBJOeCLr7SBWGkNiGPFOda7h2XJkQzyqWxET9qxPxjYq3aAc9Fax",
   // Uses Vite proxy path to completely resolve CORS browser limitations
   baseUrl: "/toobit-api" 
 };
